1f4c42268d001a691c931a8656807f41950933b9
confusezius <karsten.rh2@gmail.com>
2023-04-27 18:35:45 +0200

    included randomization

--- start_study.py
+++ start_study.py
@@ -27,6 +27,19 @@ if 'counter' not in st.session_state:
     timestr = str(datetime.datetime.now()).replace(' ', '_').replace(':', '-').replace('.', '-')
     st.session_state.path_name = f'results_{timestr}.csv'
 
+    base_file = f'assets/{filename}'
+    base_file = pd.read_csv(base_file)
+    st.session_state.prompts = np.array(list(base_file['prompt']))
+    st.session_state.image_1 = np.array(list(base_file['image_url_1']))
+    st.session_state.image_2 = np.array(list(base_file['image_url_2']))
+    st.session_state.true_indices = np.arange(len(st.session_state.prompts))
+    rand_indices = np.random.choice(len(st.session_state.prompts), len(st.session_state.prompts), replace=False)
+
+    st.session_state.true_indices = st.session_state.true_indices[rand_indices]
+    st.session_state.prompts = st.session_state.prompts[rand_indices]
+    st.session_state.image_1 = st.session_state.image_1[rand_indices]
+    st.session_state.image_2 = st.session_state.image_2[rand_indices]
+
 
 with st.sidebar:
     st.markdown("<h3>Some info about yourself:</h3>", unsafe_allow_html=True)
@@ -35,21 +48,15 @@ with st.sidebar:
     ages = ["Prefer not to say", "18-25", "26-35", "36-45", "45+"]
     sel2 = st.selectbox("Age group", ages)
     
-base_file = f'assets/{filename}'
-base_file = pd.read_csv(base_file)
-
-prompts = list(base_file['prompt'])
-image_1 = list(base_file['image_url_1'])
-image_2 = list(base_file['image_url_2'])
 
 
-def select_button(val, photo, prompt):
+def select_button(val, photo, true_index):
     st.session_state.counter += 1
     st.session_state.selections.append(val)
     model = 'Neither' if photo == 'Neither' else photo.split('/')[-2]
     st.session_state.classes.append(model)
-    st.session_state.indices.append(st.session_state.counter - 1)
-    if st.session_state.counter >= len(image_1):
+    st.session_state.indices.append(true_index)
+    if st.session_state.counter >= len(st.session_state.image_1):
         import sys
         sys.exit()
 
@@ -77,9 +84,10 @@ def end_everything():
     sys.exit()
    
 #%%     
-photo1 = image_1[st.session_state.counter]
-photo2 = image_2[st.session_state.counter]
-prompt = prompts[st.session_state.counter]
+true_index = st.session_state.true_indices[st.session_state.counter]
+photo1 = st.session_state.image_1[st.session_state.counter]
+photo2 = st.session_state.image_2[st.session_state.counter]
+prompt = st.session_state.prompts[st.session_state.counter]
 prompt = requests.get(prompt).text
  
 #%%    
@@ -87,7 +95,7 @@ prompt = requests.get(prompt).text
 st.markdown("<h2 style='text-align: center;'>Task</h2>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>Given a sentence, select which image closest reflects it. If the images are the same, choose one at random. If you are finished, please click [Done]!</h5>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>Progress</h2>", unsafe_allow_html=True)
-progr = st.progress(st.session_state.counter/len(image_1), text=f'Images seen: {st.session_state.counter}')
+progr = st.progress(st.session_state.counter/len(st.session_state.image_1), text=f'Images seen: {st.session_state.counter}')
 sel_val = st.session_state.selections[np.clip(st.session_state.counter-3, 0, None): st.session_state.counter] if st.session_state.counter else []
 sel_val = ['Option ' + str(x) for x in sel_val]
 st.markdown("<h2 style='text-align: center;'>Text Prompt</h2>", unsafe_allow_html=True)
@@ -100,9 +108,9 @@ col2, col3 = st.columns([3, 3], gap="medium")
 
     
 col2.image(photo1, caption=None)
-btt2 = col2.button(":one: I prefer this", on_click=select_button, args=[1, photo1, prompt], key='btt2')
+btt2 = col2.button(":one: I prefer this", on_click=select_button, args=[1, photo1, true_index], key='btt2')
 col3.image(photo2, caption=None)
-btt3 = col3.button(":two: I prefer this", on_click=select_button, args=[2, photo2, prompt], key='btt3')
+btt3 = col3.button(":two: I prefer this", on_click=select_button, args=[2, photo2, true_index], key='btt3')
 btt1 = st.button("**I'm done**", on_click=end_everything, key='btt1', type='primary')
 
 
