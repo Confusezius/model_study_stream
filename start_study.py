297add9cf835ea695a2aec70d89e266d05f30210
confusezius <karsten.rh2@gmail.com>
2023-05-02 23:48:03 +0200

    updated with new experiments

--- start_study.py
+++ start_study.py
@@ -9,40 +9,51 @@ import datetime
 
 #%%
 #%% 
-base_path = 'https://shyamgopalawsbucket.s3.eu-north-1.amazonaws.com'
+base_path = 'https://shyamgopalawsbucket.s3.eu-north-1.amazonaws.com/diverse_1k'
 experiments = [
     #SD14 Experiments
-    # ['sd14_fixed', 'sd14_tifa'], 
-    # ['sd14_tifa', 'sd14_imgreward'],
-    # ['ae_14','struct_diff_14', 'comp_14', 'sd14_tifa'], #Comparison between baselines improvements and seed selection on SD14.
-    # ['struct_diff_14', 'sd14_tifa'], #Comparison between baselines improvements and seed selection on SD14.
+    ['ae_14', 'sd_14_tifa'], #Comparison A&E21 & TIFA Seed Selection.    
+    ['compose_14', 'sd_14_tifa'], #Comparison A&E21 & TIFA Seed Selection.    
+    ['struct_14', 'sd_14_tifa'], #Comparison A&E21 & TIFA Seed Selection.    
+    ['sd_14_fixed', 'sd_14_tifa'], #Comparison A&E21 & TIFA Seed Selection.    
+    ['ae_14', 'sd_14_imgreward'], #Comparison A&E21 & imgreward Seed Selection.    
+    ['compose_14', 'sd_14_imgreward'], #Comparison A&E21 & imgreward Seed Selection.    
+    ['struct_14', 'sd_14_imgreward'], #Comparison A&E21 & imgreward Seed Selection.    
+    ['sd_14_fixed', 'sd_14_imgreward'], #Comparison A&E21 & imgreward Seed Selection.    
+    ['sd_14_tifa', 'sd_14_imgreward'], #Comparison tifa & imgreward Seed Selection.    
     #SD21 Experiments
-    # ['sd21_fixed', 'sd21_tifa'], 
-    # ['ae_21', 'sd21_imgreward'], #Comparison A&E21 & TIFA Seed Selection.
-    # ['ae_21', 'sd21_tifa'], #Comparison A&E21 & TIFA Seed Selection.
-    ['ae_21', 'sd21_imgreward'] #Comparison A&E21 & TIFA Seed Selection.    
+    ['ae_21', 'sd_21_tifa'], #Comparison A&E21 & TIFA Seed Selection.    
+    ['sd_21_fixed', 'sd_21_tifa'], #Comparison A&E21 & TIFA Seed Selection.    
+    ['ae_21', 'sd_21_imgreward'], #Comparison A&E21 & imgreward Seed Selection.    
+    ['sd_21_fixed', 'sd_21_imgreward'], #Comparison A&E21 & imgreward Seed Selection.    
+    ['sd_21_tifa', 'sd_21_imgreward'] #Comparison A&E21 & imgreward Seed Selection.    
 ]
 
 img_files = [f'{i}.jpg' for i in range(1, 1001)]
 text_files = [f'{i}.txt' for i in range(1, 1001)]
 prompt_folder = 'prompts'
 
-if 'counter' not in st.session_state:
-    # select experiment
-    exp_idx = np.random.choice(len(experiments))
-    exps = experiments[exp_idx]
-    
-    st.session_state.prompts = np.array([base_path + '/' + prompt_folder + '/' + x for x in text_files])
-    st.session_state.true_indices = np.arange(len(st.session_state.prompts))
-    rand_indices = np.random.choice(len(st.session_state.prompts), len(st.session_state.prompts), replace=False)
-    st.session_state.prompts = st.session_state.prompts[rand_indices]
-    st.session_state.image_dict = {mode: np.array([base_path + '/' + mode + '/' + x for x in img_files])[rand_indices] for mode in exps}
-    st.session_state.true_indices = st.session_state.true_indices[rand_indices]
-    
-    st.session_state.exps = exps
-    # print(exps)
+# select experiment
+exp_idx = np.random.choice(len(experiments))
+prompt_idx = np.random.choice(len(text_files))
+exps = experiments[exp_idx]
+prompt = base_path + '/' + prompt_folder + '/' + text_files[prompt_idx]
+image_dict = {mode: base_path + '/' + mode + '/' + img_files[prompt_idx] for mode in exps}
 
-layout = 'wide' if len(st.session_state.exps) > 3 else 'centered'
+# st.session_state.prompts = np.array([base_path + '/' + prompt_folder + '/' + x for x in text_files])
+# st.session_state.true_indices = np.arange(len(st.session_state.prompts))
+# rand_indices = np.random.choice(len(st.session_state.prompts), len(st.session_state.prompts), replace=False)
+# st.session_state.prompts = st.session_state.prompts[rand_indices]
+# st.session_state.image_dict = {mode: np.array([base_path + '/' + mode + '/' + x for x in img_files])[rand_indices] for mode in exps}
+# st.session_state.true_indices = st.session_state.true_indices[rand_indices]
+
+# st.session_state.exps = exps
+# print(exps)
+
+    
+# if 'counter' not in st.session_state:   
+
+layout = 'wide' if len(exps) > 3 else 'centered'
 st.set_page_config(layout=layout, page_title="Compare Generated Images")
 
 #%%
@@ -59,6 +70,7 @@ if 'counter' not in st.session_state:
     st.session_state.indices = []
     st.session_state.exp_list = []
     counter = 0
+    st.session_state.num_prompts_per_call = 250   
     timestr = str(datetime.datetime.now()).replace(' ', '_').replace(':', '-').replace('.', '-')
     st.session_state.path_name = f'results_{timestr}.csv'
 
@@ -81,11 +93,10 @@ def select_button(val, photo, true_index):
     st.session_state.counter += 1
     st.session_state.selections.append(val)
     model = 'Neither' if photo == 'Neither' else photo.split('/')[-2]
-    # print(model)
     st.session_state.classes.append(model)
     st.session_state.indices.append(true_index)
-    st.session_state.exp_list.append('__'.join(st.session_state.exps))
-    if st.session_state.counter >= len(st.session_state.prompts):
+    st.session_state.exp_list.append('__'.join(exps))
+    if st.session_state.counter >= st.session_state.num_prompts_per_call:
         end_everything()
 
 #AWS: eu-central-1    
@@ -115,17 +126,18 @@ def end_everything():
     sys.exit()
    
 #%%     
-true_index = st.session_state.true_indices[st.session_state.counter]
-prompt = st.session_state.prompts[st.session_state.counter]
+# prompt_idx = st.session_state.true_indices[st.session_state.counter]
+# prompt = st.session_state.prompts[st.session_state.counter]
 prompt = f'"{requests.get(prompt).text}"'
-photos = [st.session_state.image_dict[exp][st.session_state.counter] for exp in st.session_state.exps]
+photos = [image_dict[exp] for exp in exps]
+# photos = [st.session_state.image_dict[exp][st.session_state.counter] for exp in st.session_state.exps]
  
 #%%    
 # Get list of images in folder
 st.markdown("<h2 style='text-align: center;'>Task</h2>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size:20px'>Select which image most <b>faithfully</b> reflects the sentence. If the images are the same, choose one at random. If you are finished, please click <b>[Done]</b>!</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size:20px'>Select which image most <b>faithfully</b> reflects the sentence. See left for more info. If the images are the same, choose one at random. If you are finished, please click <b>[Done]</b>!</p>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>Progress</h2>", unsafe_allow_html=True)
-progr = st.progress(st.session_state.counter/len(st.session_state.prompts), text=f'Images seen: {st.session_state.counter} | {len(st.session_state.prompts)}')
+progr = st.progress(st.session_state.counter/st.session_state.num_prompts_per_call, text=f'Images seen: {st.session_state.counter} | {st.session_state.num_prompts_per_call}')
 sel_val = st.session_state.selections[np.clip(st.session_state.counter-3, 0, None): st.session_state.counter] if st.session_state.counter else []
 sel_val = ['Option ' + str(x) for x in sel_val]
 st.markdown("<h2 style='text-align: center;'>Text Prompt</h2>", unsafe_allow_html=True)
@@ -134,7 +146,8 @@ st.write('')
 st.write('')
 st.write('')
 
-cols = st.columns(len(st.session_state.exps), gap="medium")
+cols = st.columns(len(exps), gap="medium")
+# cols = st.columns(len(st.session_state.exps), gap="medium")
 # Randomly permute columns.
 col_idcs = np.random.choice(len(cols), len(cols), replace=False)
 cols = [cols[i] for i in col_idcs]
@@ -143,9 +156,10 @@ emoji_dict = {
     0: ':one:', 1: ':two:', 2: ':three:', 3: ':four:', 4: ':five:',
     5: ':six:', 6: ':seven:', 7: ':eight:', 8: ':nine:'
 }
-for i, exp in enumerate(st.session_state.exps):
+
+for i, exp in enumerate(exps):
     cols[i].image(photos[i], caption=None, use_column_width=True)
-    _ = cols[i].button(f"{emoji_dict[col_idcs[i]]} I prefer this", on_click=select_button, args=[i, photos[i], true_index], key=f'btt{i}')
+    _ = cols[i].button(f"{emoji_dict[col_idcs[i]]} I prefer this", on_click=select_button, args=[i, photos[i], prompt_idx], key=f'btt{i}')
 btt_done = st.button("**I'm done**", on_click=end_everything, key='btt_done', type='primary')
 
 
